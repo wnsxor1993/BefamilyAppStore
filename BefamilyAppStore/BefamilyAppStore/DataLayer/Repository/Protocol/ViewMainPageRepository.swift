a0534eda09ff11afce1784d18006c9827d01672a
wnsxor1993 <wnsxor1993@naver.com>
2022-08-06 16:56:10 +0900

    feat: ViewMainRepository protocol 생성 및 구체 타입 JSONConverter 활용 구문 추가

---
+++ BefamilyAppStore/BefamilyAppStore/DataLayer/Repository/Protocol/ViewMainPageRepository.swift
@@ -0,0 +1,14 @@
+//
+//  ViewMainPageRepository.swift
+//  BefamilyAppStore
+//
+//  Created by juntaek.oh on 2022/08/06.
+//
+
+import Foundation
+import RxSwift
+
+protocol ViewMainPageRepository {
+    
+    func fetchDTO() -> Observable<MainPageDTO>
+}
--- BefamilyAppStore/BefamilyAppStore/DataLayer/Repository/ViewDefaultMainPageRepository.swift
+++ BefamilyAppStore/BefamilyAppStore/DataLayer/Repository/ViewDefaultMainPageRepository.swift
@@ -13,7 +13,7 @@ final class ViewDefaultMainPageRepository {
     let networkService = NetworkService()
     let disposeBag = DisposeBag()
     
-    func requestMainPageData() -> Observable<MainPageDTO> {
+    func fetchDTO() -> Observable<MainPageDTO> {
         
         return Observable.create { [weak self] observer -> Disposable in
             guard let self = self else {
@@ -23,7 +23,17 @@ final class ViewDefaultMainPageRepository {
             
             self.networkService.request(endPoint: .mainPage)
                 .subscribe { data in
+                    let jsonConverter = JSONConverter<PagesArrayDTO>()
+                    guard let pagesArrayDTO = jsonConverter.decode(data: data) else {
+                        observer.onError(DataError.decodingError)
+                        return
+                    }
                     
+                    if pagesArrayDTO.resultCount == 1 {
+                        observer.onNext(pagesArrayDTO.results[0])
+                    } else {
+                        observer.onError(ExtraError.mainPageDTOCountNotOne)
+                    }
                     
                 } onFailure: { _ in
                     observer.onError(DataError.noData)
